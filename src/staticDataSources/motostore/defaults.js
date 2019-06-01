f72767f52213834f936446099029ff96824edb70
Yegor Shapoval <>
2019-06-01 15:45:01 +0300

    Added staticDataSources

---
+++ src/staticDataSources/motostore/defaults.js
@@ -0,0 +1,39 @@
+const motocycles = [
+  {
+    slug: "cb125f",
+    title: "Honda CB 125 F",
+    description: "Твій шлях до свободи! Малокубатурний мотоцикл CB125 популярний і серед початківців, і серед досвідчених мотоциклістів.Елегантний і сучасний, він має неповторний стиль і харизму.",
+    price: 2000,
+    imageUrl: "https://honda.ua/content/images/cb125f.jpg",
+  },
+  {
+    slug: "goldwing_gl1800",
+    title: "Honda Gold Wing GL-1800",
+    description: "Неперевершений турінговий мотоцикл Honda став ще більш універсальним з появою останньої версії легендарної моделі Gold Wing, яка не тільки задає нові граничні характеристики для турінгових мотоциклів, а й переписує саму концепцію поїздок на таких мотоциклах.",
+    price: 40000,
+    imageUrl: "https://cdn0.riastatic.com/photosnew/auto/photo/honda_gold-wing__240675910fx.webp",
+  },
+  {
+    slug: "cb1100ex",
+    title: "Honda CB 1100 EX",
+    description: "Абсолютно новий Honda CB1100 створений для сучасного мотоцикліста, який при цьому не забуває про класичні цінності мото світу.",
+    price: 16000,
+    imageUrl: "https://honda.ua/content/catalog/gallery/item_885_5262_orig.jpg",
+  },
+  {
+    slug: "nc750x",
+    title: "Honda NC 750 X",
+    description: "З моменту появи в 2012, NC750X завоював популярність в Європі, увійшовши в десятку найбільш продаваних в Європі мотоциклів.",
+    price: 10000,
+    imageUrl: "https://honda.ua/content/catalog/gallery/item_871_5062_orig.jpg",
+  },
+  {
+    slug: "cbr1000rr",
+    title: "Honda CBR 1000 RR",
+    description: "Honda CBR1000RR Fireblade виводить «Абсолютний Контроль» на новий рівень. На 90%нові основні компоненти. На14% покращене співвідношення ваги та потужності – безпрецедентне досягнення для Fireblade – завдяки меншій на 15 кг вазі та додатковим 8 кВт загальної потужності.",
+    price: 12000,
+    imageUrl: "https://honda.ua/content/catalog/gallery/item_988_5371_orig.jpg",
+  },
+];
+
+export default motocycles;
