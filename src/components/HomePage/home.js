0b20b0f39125cea333717ca5601beb92308fcaa7
Yegor Shapoval <>
2019-06-20 12:05:05 +0300

    Added categories and breadcrumbs

--- src/components/HomePage/home.js
+++ src/components/HomePage/home.js
@@ -1,22 +1,39 @@
 import React from 'react';
-import { connect } from 'react-redux'
-import MotoListItem from '../MotoListItem/motolistitem'
+import { connect } from 'react-redux';
+import "./home.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 
 
-function Home({ motos }) {
+function Home() {
   return (
+    <>
+      <div>
+        <Breadcrumb listClassName="crumbs">
+          <BreadcrumbItem tag="a" active>Honda MotorCycles</BreadcrumbItem>
+        </Breadcrumb>
+      </div>
     <div className="motos-container">
-      {motos.map((moto, index) => (
-        <MotoListItem
-          key={index}
-          title={moto.title}
-          image={moto.imageUrl}
-          desc={moto.description}
-          price={moto.price}
-          slug={moto.slug}
-        />
-      ))}
+      <Link to="/category/sport" className="motocategory">
+      <div>
+        <img src="https://images.vexels.com/media/users/3/152654/isolated/preview/e5694fb12916c00661195c0a833d1ba9-sports-bike-icon-by-vexels.png" className="iconimg"></img>
+        <p className="categorytitle">Sport</p>
+      </div>
+      </Link>
+      <Link to="/category/classic" className="motocategory">
+        <div>
+          <img src="https://images.vexels.com/media/users/3/152376/isolated/preview/c5f4390c69b1c5113cec24502b46cc8e-classic-motorcycle-icon-by-vexels.png" className="iconimg"></img>
+          <p className="categorytitle">Classic</p>
+      </div>
+      </Link>
+      <Link to="/category/lowcubature" className="motocategory">
+      <div>
+        <img src="https://www.shareicon.net/download/2016/08/18/809623_transportation_512x512.png" className="iconimg"></img>
+          <p className="categorytitle">Low-Cubature</p>
+      </div>
+      </Link>
     </div>
+    </>
   )
 }
 const mapStateToProps = (state) => {
@@ -24,4 +41,4 @@ const mapStateToProps = (state) => {
     motos: state.items
   }
 }
-export default connect(mapStateToProps, null)(Home)
+export default connect(mapStateToProps, null)(Home);
